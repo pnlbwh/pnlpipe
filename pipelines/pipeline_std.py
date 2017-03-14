d310f4cf449e33ed732df4d3d153bc0400d94539
reckbo <reckbo@bwh.harvard.edu>
2017-03-14 05:52:27 +0000

    ENH: add dwied key option to std pipeline

--- pipelines/pipeline_std.py
+++ pipelines/pipeline_std.py
@@ -1,28 +1,38 @@
+import sys
 from pipelines.pnlnodes import StrctXc, DwiXc, FsInDwiDirect, FreeSurferUsingMask, T1wMaskMabs, DwiMaskHcpBet, DwiEd, UkfDefault, Wmql, TractMeasures, T2wMaskRigid, DwiEpi, DoesNotExistException, assertInputKeys
 from pipelib import Src
 import pipelib
 
 def makePipeline(caseid,
-                 dwiKey,
                  t1Key,
+                 dwiKey='',
                  hash_UKFTractography='421a7ad',
                  hash_tract_querier='e045eab',
                  hash_BRAINSTools='41353e8',
                  hash_trainingDataT1AHCC='d6e5990',
+                 dwiedKey='',  # provide if hcp or buddi used
                  dwimaskKey=''
                  ):
 
     """Makes the PNL's standard pipeline. """
     pipeline = { 'name' : "standard PNL pipeline" }
-    assertInputKeys(pipeline['name'], [dwiKey, t1Key])
+    assertInputKeys(pipeline['name'], [t1Key])
 
     pipeline['t1'] = Src(caseid, t1Key)
-    pipeline['dwi'] = Src(caseid, dwiKey)
+
+    # get DWI
+    if dwiKey:
+        pipeline['dwi'] = Src(caseid, dwiKey)
+        pipeline['dwixc'] = DwiXc(caseid, pipeline['dwi'], hash_BRAINSTools) # works on nrrd or nii
+        pipeline['dwied'] = Src(caseid, dwiedKey) if pipelib.INPUT_PATHS.get(
+            dwiedKey) else DwiEd(caseid, pipeline['dwixc'], hash_BRAINSTools)
+    elif dwiedKey:
+        pipeline['dwied'] = Src(caseid, dwiedKey)
+    else:
+        print("pipeline_std.py: Error: either 'dwiKey' or 'dwiedKey' must be provided in 'params.std'")
+        sys.exit(1)
 
     pipeline['t1xc'] = StrctXc(caseid, pipeline['t1'], hash_BRAINSTools)
-    # run DwiXc first as it's able to convert a DWI nifti to nrrd
-    pipeline['dwixc'] = DwiXc(caseid, pipeline['dwi'], hash_BRAINSTools)
-    pipeline['dwied'] = DwiEd(caseid, pipeline['dwixc'], hash_BRAINSTools)
 
     pipeline['dwimask'] = Src(
         caseid, dwimaskKey) if pipelib.INPUT_PATHS.get(
