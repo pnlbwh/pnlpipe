16811116d52703580dfea8df9fcc29ddbc314b09
reckbo <reckbo@bwh.harvard.edu>
2017-03-14 22:19:11 -0400

    STY: get mrtrix3 working on cluster

---
+++ software/erisone_mrtrix3_buildenv.sh
@@ -0,0 +1,2 @@
+module load gcc
+export EIGEN_CFLAGS="-isystem /source/eigen/3.2.1//include/eigen3/"
--- software/mrtrix3.py
+++ software/mrtrix3.py
@@ -8,13 +8,15 @@ from plumbum.cmd import wget, tar
 DEFAULT_VERSION = '97e4b3b'
 
 def make(version=DEFAULT_VERSION):
+    if checkExists(getPath(version)):
+        sys.exit(1)
     with TemporaryDirectory() as tmpdir, local.cwd(tmpdir):
         tmpdir = local.path(tmpdir)
         repo = downloadGithubRepo('MRtrix3/mrtrix3', version)
         with local.cwd(repo):
             os.system('./configure')
             os.system('./build')
-            local.path('release').move(getPath(version))
+            os.system('mv ./release {}'.format(getPath(version)))
 
 def getPath(version=DEFAULT_VERSION):
     return getSoftDir() / ('mrtrix3-' + version)
