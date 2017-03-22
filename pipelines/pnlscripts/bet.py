27fa04fab2bec221ba0cec7becd21978f75e3e7f
reckbo <reckbo@bwh.harvard.edu>
2017-03-22 09:27:35 +0000

    ENH: add nifti dwi to bet.py

--- pipelines/pnlscripts/bet.py
+++ pipelines/pnlscripts/bet.py
@@ -2,15 +2,21 @@
 from util.scripts import bse_py
 from plumbum.cmd import bet, ConvertBetweenFileFormats
 from plumbum import cli, FG
-from util import Nrrd, ExistingNrrd, TemporaryDirectory
+from util import Nrrd, ExistingNrrd, ExistingNrrdOrNifti, TemporaryDirectory
 import logging
+
+def nifti(f):
+    return '.nii' in f.suffixes
+def nrrd(f):
+    return '.nrrd' in f.suffixes or '.nhdr' in f.suffixes
+
 
 class App(cli.Application):
     force = cli.Flag(['--force'], help='Force overwrite if output already exists',mandatory=False,default=False)
     threshold = cli.SwitchAttr('-f', help='Bet fractional intensity threshold', default=0.1)
     dwi = cli.SwitchAttr(
         ['-i', '--input'],
-        ExistingNrrd,
+        ExistingNrrdOrNifti,
         help='Input DWI',
         mandatory=True)
     out = cli.SwitchAttr(
@@ -25,12 +31,14 @@ class App(cli.Application):
             sys.exit(1)
         with TemporaryDirectory() as tmpdir:
             #nii = tmpdir / 'dwi.nii.gz'
-            bse = tmpdir / 'bse.nrrd'
-            bsenii = tmpdir / 'bse.nii.gz'
-            bse_py['-i', self.dwi, '-o', bse] & FG
-            ConvertBetweenFileFormats[bse, bsenii] & FG
-            #convertdwi_py['-i', self.dwi, '-o', nii] & FG
-            bet[bsenii, tmpdir / 'dwi', '-m', '-f', self.threshold] & FG
+            if nrrd(self.dwi):
+                bse = tmpdir / 'bse.nrrd'
+                bse_py['-i', self.dwi, '-o', bse] & FG
+                bsenii = tmpdir / 'bse.nii.gz'
+                ConvertBetweenFileFormats[bse, bsenii] & FG
+                bet[bsenii, tmpdir / 'dwi', '-m', '-f', self.threshold] & FG
+            else: #nifti
+                bet[self.dwi, tmpdir / 'dwi', '-m', '-f', self.threshold] & FG
             ConvertBetweenFileFormats[tmpdir / 'dwi_mask.nii.gz', self.out] & FG
 
 if __name__ == '__main__':
