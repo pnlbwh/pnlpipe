dd694657209dbb381095a3cc2586956c79f2a35a
reckbo <reckbo@bwh.harvard.edu>
2017-03-23 01:17:55 +0000

    BUG/ENH: add missing func to mrtrix3.py, add dice coefficient to slicermask

--- pipelines/pipeline_slicermask.py
+++ pipelines/pipeline_slicermask.py
@@ -30,15 +30,37 @@ class DwiMaskSlicer(GeneratedNode):
             maskfilter['-scale', 2, self.path(), 'clean', 'otsumask.nii',
                        '-force'] & FG
 
+class DiceCoefficient(GeneratedNode):
+    def __init__(self, caseid, maskManual, mask, hash_BRAINSTools):
+        self.deps = [maskManual, mask]
+        self.params = [version_BRAINSTools]
+        self.ext = '.txt'
+        GeneratedNode.__init__(self, locals())
+
+    def build(self):
+        from plumbum.cmd import ImageMath
+        needDeps(self)
+        with TemporaryDirectory() as tmpdir, software.BRAINSTools(self.version_BRAINSTools):
+            tmptxt = tmpdir / 'dice.txt'
+            ImageMath[3, tmptxt, "DiceAndMinDistSum", self.maskManual, self.mask]
+            with open(tmptxt, 'r') as f:
+                coeff = f.read().split('')[-1]
+            with open(self.path(), 'w') as f:
+                f.write(coeff)
+
 
 def makePipeline(caseid,
                  dwiPathKey='dwi',
+                 dwimaskManualPathKey='dwimaskManual',
                  version_Slicer='4.7.0',
+                 hash_BRAINSTools='41353e8',
                  hash_mrtrix3='97e4b3b'):
     pipeline = {'_name': "dwi masking test"}
     pipeline['dwi'] = Src(caseid, dwiPathKey)
+    pipeline['dwimaskManual'] = Src(caseid, dwimaskManualPathKey)
     pipeline['dwimask'] = DwiMaskSlicer(caseid, pipeline['dwi'],
                                         version_Slicer, hash_mrtrix3)
+    pipeline['dice'] = DiceCoefficient(caseid, pipeline['dwimaskManual'], pipeline['dwimask'], hash_BRAINSTools)
     return pipeline
 
 # /rfanfs/pnl-zorro/projects/Lyall_R03/Slicer-build2/Slicer-build/Slicer --launch DiffusionWeightedVolumeMasking  dwi.nhdr dwi_b0.nrrd dwi_OTSUtensormask.nrrd --baselineBValueThreshold 1000 --removeislands
