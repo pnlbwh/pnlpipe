9e4f68f40fe3bb017027df782a9e1df420f50688
reckbo <reckbo@bwh.harvard.edu>
2017-03-30 04:35:40 +0000

    STY: add missing file from last commit

--- pipelines/pnlscripts/summarizeTractMeasures.py
+++ pipelines/pnlscripts/summarizeTractMeasures.py
@@ -8,31 +8,47 @@ else:
     from io import StringIO
 
 def summarize(df):
-    INTRUST=StringIO(""""tract","FAmean_mean","FAmean_sd","num_mean","num_sd","count"
-af.left,714.182544932399,30.4676278440711,433.140625,257.950520516233,384
-af.right,696.470852774055,34.8401036745462,441.361038961039,290.948014111773,385
-ioff.left,714.685249908589,30.4187930443161,131.124675324675,105.584410539235,385
-ioff.right,707.633674637833,30.0749770765204,201.296875,135.805998900886,384
-slf_i.left,591.851359327127,40.0703509639063,452.316883116883,264.787696699815,385
-slf_i.right,580.02563236122,39.589693983944,278.345454545455,200.974326294543,385
-slf_ii.left,659.341421059491,32.6647141147986,339.862337662338,257.711077039572,385
-slf_ii.right,660.085906472326,30.9626825136909,356.722077922078,271.30096133385,385
-slf_iii.left,666.308324741928,29.5571559220112,656.311688311688,344.504930863216,385
-slf_iii.right,660.472731188003,30.0607433890063,856.412987012987,439.223181852611,385
-uf.left,589.85265447185,42.8613369562814,195.744125326371,174.371158305798,383
-uf.right,565.9559476129,43.1105907847185,142.316883116883,126.637537840325,385""")
+    INTRUST=StringIO(""""algo","tract","FAmean_mean","FAmean_sd","num_mean","num_sd","count"
+INTRuST,af.left,714.182544932399,30.4676278440711,433.140625,257.950520516233,384
+INTRuST,af.right,696.470852774055,34.8401036745462,441.361038961039,290.948014111773,385
+INTRuST,ioff.left,714.685249908589,30.4187930443161,131.124675324675,105.584410539235,385
+INTRuST,ioff.right,707.633674637833,30.0749770765204,201.296875,135.805998900886,384
+INTRuST,slf_i.left,591.851359327127,40.0703509639063,452.316883116883,264.787696699815,385
+INTRuST,slf_i.right,580.02563236122,39.589693983944,278.345454545455,200.974326294543,385
+INTRuST,slf_ii.left,659.341421059491,32.6647141147986,339.862337662338,257.711077039572,385
+INTRuST,slf_ii.right,660.085906472326,30.9626825136909,356.722077922078,271.30096133385,385
+INTRuST,slf_iii.left,666.308324741928,29.5571559220112,656.311688311688,344.504930863216,385
+INTRuST,slf_iii.right,660.472731188003,30.0607433890063,856.412987012987,439.223181852611,385
+INTRuST,uf.left,589.85265447185,42.8613369562814,195.744125326371,174.371158305798,383
+INTRuST,uf.right,565.9559476129,43.1105907847185,142.316883116883,126.637537840325,385""")
 
     mask = df.tract.str.lower().apply( lambda x: any([ y in x for y in ['af','uf','slf','ioff']]))
-    agg = {'FA_mean': {'FAmean_mean': 'mean' ,'FAmean_sd': 'std', 'count':'count'}
-           ,'num': {'num_mean':'mean', 'num_sd': 'std'}
-    }
-    df = df[mask].filter(items=['FA_mean','num','tract']).groupby(['algo','tract']).agg(agg)
-    df.columns = df.columns.droplevel()
-    df = df[['FAmean_mean','FAmean_sd','num_mean','num_sd','count']]
-
+    # agg = {'FA_mean': {'FAmean_mean': 'mean' ,'FAmean_sd': 'std', 'count':'count'}
+    #        ,'num': {'num_mean':'mean', 'num_sd': 'std'}
+    # }
+    agg = {'FA_mean': ['mean', 'std','count']
+           ,'num': ['mean', 'std']}
+    df = df[mask].filter(items=['FA_mean','num','tract','algo']).groupby(['tract','algo'], as_index=False).agg(agg)
+    # df = df[mask].filter(items=['FA_mean','num','tract','algo']).groupby(['tract','algo']).agg(agg)
+    # print(df.columns)
+    dfn = pd.DataFrame()
+    dfn['tract'] = df['tract']
+    dfn['algo'] = df['algo']
+    dfn['FAmean_mean'] = df['FA_mean']['mean']
+    dfn['FAmean_sd'] = df['FA_mean']['std']
+    dfn['num_mean'] = df['num']['mean']
+    dfn['num_sd'] = df['num']['std']
+    dfn['count'] = df['FA_mean']['count']
+    dfn = dfn.set_index('algo')
+    # dfn = df['tract']
+    # df.columns = df.columns.droplevel(0)
+    # df.columns = df.columns.droplevel(0)
+    # df.reset_index(inplace=True)
+    # df = df[['algo','tract','FAmean_mean','FAmean_sd','num_mean','num_sd','count']]
+    # df.reset_index()
+    # df.columns = df.columns.get_level_values(0)
+    print(dfn)
     dfintrust= pd.read_csv(INTRUST, sep=",",index_col=0)
-
-    print(df)
     print("Compare to INTRuST:")
     print(dfintrust)
 
