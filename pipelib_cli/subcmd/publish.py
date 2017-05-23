3235cedafcd5d4bd75f87aeb61901c8e156a4aaa
reckbo <reckbo@bwh.harvard.edu>
2017-05-23 04:27:48 +0000

    ENH: update publish subcommand

--- pipelib_cli/subcmd/publish.py
+++ pipelib_cli/subcmd/publish.py
@@ -1,4 +1,4 @@
-from plumbum import cli, local
+from plumbum import cli, local, FG
 from pipelib_cli import readAndSetSrcPaths
 from pipelib_cli.params import readComboPaths
 import sys
@@ -10,21 +10,25 @@ import csv
 import yaml
 from collections import OrderedDict
 
-PUBLISH_CONFIG_FILE = 'publish.yml'
-PNLPROJECTSMETA = 'PNLPROJECTSMETA'
+PROJECT_YML = 'project.yml'
+PROJECT_INFO_YML = 'projectInfo.yml'
+PNL_PROJECTS_DB = 'PNL_PROJECTS_DB'
+
 
 def csvFromDict(d):
     s = ""
-    hdr = 'project,projectPath,grantId,paramId,caselist,param,paramValue'
+    hdr = 'project,grantId,paramId,caselist,param,paramValue'
     row = ','.join(d.values())
     return hdr + '\n' + row
 
 
 def readProjectInfo():
-    with open(PUBLISH_CONFIG_FILE, 'r') as f:
+    with open(PROJECT_YML, 'r') as f:
         return yaml.load(f)
 
+
 class Publish(cli.Application):
+    """Makes project summary file and pushes to central project database."""
 
     def main(self, *args):
         if args:
@@ -33,72 +37,89 @@ class Publish(cli.Application):
         if self.nested_command:
             return
 
-        readAndSetSrcPaths()
-        combos = readComboPaths(self.parent.paramsFile)
-        projectInfo = readProjectInfo()
-
-        paramsCsv = '_{}_publish_params.csv'.format(self.parent.name)
-        pathsCsv = '_{}_publish_paths.csv'.format(self.parent.name)
-
-        with open(paramsCsv, 'w') as fparamsCsv:
-            csvwriter = csv.writer(fparamsCsv)
-            hdr = ['project', 'projectPath', 'description', 'paramId'
-                ,'param', 'paramValue']
-            csvwriter.writerow(hdr)
-            with open(pathsCsv, 'w') as fpathsCsv:
-                hdr = ['project', 'projectPath', 'paramId', 'pathKey', 'caseid',
-                        'path', 'exists']
-                csvwriter2 = csv.writer(fpathsCsv)
-                csvwriter2.writerow(hdr)
-
-                for combo in combos:
-                    for k, v in combo['paramCombo'].items():
-                        csvwriter.writerow([projectInfo['project'],
-                                            projectInfo['projectPath'],
-                                            projectInfo['description'], combo['paramId'], k,
-                                            v])
-                    for pathKey, subjectPaths in combo['paths'].items():
-                        for subjectPath in subjectPaths:
-                            csvwriter2.writerow(
-                                [projectInfo['project'], projectInfo['projectPath'],
-                                combo['paramId'], pathKey, subjectPath.caseid,
-                                subjectPath.path, subjectPath.path.exists()])
-            print("Made '{}'".format(paramsCsv))
-            print("Made '{}'".format(pathsCsv))
-
 
 @Publish.subcommand("init")
 class Init(cli.Application):
+    """Makes 'project.yml'"""
+
     def main(self):
+
+        if local.path(PROJECT_YML).exists():
+            msg = "'{}' already exists, to recreate it delete it first.".format(
+                PROJECT_YML)
+            print(msg)
+            sys.exit(1)
+
         represent_dict_order = lambda self, data: self.represent_mapping('tag:yaml.org,2002:map', data.items())
         yaml.add_representer(OrderedDict, represent_dict_order)
 
-        with open(PUBLISH_CONFIG_FILE, 'w') as f:
-            f.write('project: {}\n'.format(local.cwd.dirname.__str__()))
-            f.write('projectPath: {}\n'.format(local.cwd.__str__()))
-            f.write('grantId: {}\n'.format(''))
-            f.write('description: {}\n'.format(''))
+        if local.path(PROJECT_INFO_YML).exists():
+            print(
+                "Found '{}', using that to populate the 'projectInfo' field in '{}'".format(
+                    PROJECT_INFO_YML, PROJECT_YML))
+            with open(PROJECT_INFO_YML, 'r') as f:
+                pi = yaml.load(f)
+            mandatoryFields = ['projectName', 'grantId', 'description']
+            if not set(mandatoryFields) <= set(pi.keys()):
+                errmsg = "'{}' is missing one of the required fields: {}".format(
+                    PROJECT_INFO_YML, ', '.join(mandatoryFields))
+                raise Exception(errmsg)
+        else:
+            print(
+                "Didn't find '{}', using defaults to populate 'projectInfo' field in '{}'".format(
+                    PROJECT_INFO_YML, PROJECT_YML))
+            pi = {'grandId': ''}
+            pi['projectName'] = local.cwd.dirname.__str__()
+            pi['description'] = ''
 
+        result = {}
+        result['projectInfo'] = pi
 
-        print("Made '{}'".format(PUBLISH_CONFIG_FILE))
+        readAndSetSrcPaths()
+        combos = readComboPaths(self.parent.parent.paramsFile)
+
+        pipelines = []
+        for combo in combos:
+            pipeline = {}
+            pipeline['parameters'] = combo['paramCombo']
+            pipeline['paths'] = {}
+            pipeline['paths']['caselist'] = combo['caseids']
+            for pathKey, subjectPaths in combo['paths'].items():
+                s = subjectPaths[0]
+                pipeline['paths'][pathKey] = s.path.__str__()
+                pipeline['paths']['caseid'] = s.caseid
+            pipelines.append(pipeline)
+        result['pipelines'] = pipelines
+
+        with open(PROJECT_YML, 'w') as f:
+            yaml.dump(result, f, default_flow_style=False)
+        print("Made '{}'".format(PROJECT_YML))
+        if not local.path(PROJECT_INFO_YML).exists():
+            print("Now customize the 'projectInfo' section and save.".format(
+                PROJECT_YML))
 
 
 @Publish.subcommand("push")
 class Push(cli.Application):
+    """Copies project.yml to central project database"""
+
+    def readFullName(self):
+        return local.cwd.__str__().replace('/', '-')[1:] + '.yml'
 
     def main(self):
         import os
-        centralRepo = os.environ.get(PNLPROJECTSMETA, None)
+        from plumbum.cmd import scp
+
+        centralRepo = os.environ.get(PNL_PROJECTS_DB, None)
+
         if not centralRepo:
-            errmsg = "Set '{}' environment variable first.".format(PNLPROJECTSMETA)
+            errmsg = "Set '{}' environment variable first.".format(
+                PNL_PROJECTS_DB)
             raise Exception(errmsg)
 
-        paramsCsv = local.path('_{}_publish_params.csv'.format(self.parent.parent.name))
-        pathsCsv = local.path('_{}_publish_paths.csv'.format(self.parent.parent.name))
+        destName = centralRepo + '/' + self.readFullName()
 
-        if not paramsCsv.exists():
-            errmsg = "'{}' does not exist, run 'publish' command first".format(paramsCsv)
-            raise Exception(errmsg)
-        if not pathsCsv.exists():
-            errmsg = "'{}' does not exist, run 'publish' command first".format(pathsCsv)
-            raise Exception(errmsg)
+        print("Copy project.yml to '{}'".format(destName))
+        scp['project.yml', destName] & FG
+        print("Successfully copied '{}' to '{}'".format('project.yml',
+                                                        destName))
