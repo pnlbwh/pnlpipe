daed1e5172478812a90841e617118fccbfd12528
reckbo <reckbo@bwh.harvard.edu>
2017-07-25 16:40:59 +0000

    ENH/BUG: update export to match new pnldash format

--- pnlpipe_cli/cmd/export.py
+++ pnlpipe_cli/cmd/export.py
@@ -7,13 +7,13 @@ import sys
 import yaml
 from collections import OrderedDict
 
-PROJECT_YML = 'pnlproj.yml'
-PROJECT_INFO_YML = 'projectInfo.yml'
-PNL_PROJECTS_DB = 'PNL_PROJECTS_DB'
+PROJECT_YML = 'pnldash.yml'
+PROJECT_INFO_YML = 'pnldash-header.yml'
+PNL_PROJECTS_DB = 'PNLDASH_DB'
 
 
 class Export(cli.Application):
-    """Makes 'pnlproj.yml' from your configured pipeline(s)."""
+    """Makes 'pnldash.yml' from your configured pipeline(s)."""
 
     force = cli.Flag(['-f', '--force'], default=False, help='force overwrite')
 
@@ -37,6 +37,7 @@ class Export(cli.Application):
         yaml.add_representer(OrderedDict, represent_dict_order)
 
         projdicts = []
+        result = {}
 
         for subprojectyml in (local.cwd // 'pnlproj-*yml'):
             with open(subprojectyml, 'r') as f:
@@ -47,28 +48,26 @@ class Export(cli.Application):
 
         if local.path(PROJECT_INFO_YML).exists():
             print(
-                "Found '{}', using that to populate the 'projectInfo' field in '{}'".format(
+                "Found '{}', adding its contents to the beginning of '{}'".format(
                     PROJECT_INFO_YML, PROJECT_YML))
             with open(PROJECT_INFO_YML, 'r') as f:
-                pi = yaml.load(f)
-            mandatoryFields = ['grantId', 'description']
-            if not set(mandatoryFields) <= set(pi.keys()):
+                result = yaml.load(f)
+            mandatoryFields = ['grantId', 'description', 'name']
+            if not set(mandatoryFields) <= set(result.keys()):
                 errmsg = "'{}' is missing one of the required fields: {}".format(
                     PROJECT_INFO_YML, ', '.join(mandatoryFields))
                 raise Exception(errmsg)
         else:
             print(
-                "Didn't find '{}', using defaults to populate 'projectInfo' field in '{}'".format(
+                "Didn't find '{}', using defaults to populate the header fields in '{}'".format(
                     PROJECT_INFO_YML, PROJECT_YML))
-            pi = {'grantId': ''}
-            pi['description'] = ''
-
-        result = {}
-        result['projectInfo'] = pi
+            result = {'name': ''}
+            result['description'] = ''
+            result['grantId'] = ''
 
         srcPathsStr = '\n'.join(['{:<8}:  {}'.format(k, v)
                                  for k, v in pnlpipe_config.INPUT_KEYS.items()])
-        result['projectInfo']['description'] = result['projectInfo']['description'] + \
+        result['description'] = result['description'] + \
                                                '\n\n\nInput Keys (from {}):\n\n'.format('pnlpipe_config.py') + srcPathsStr
 
         for pipeline_name in pipeline_names:
@@ -98,4 +97,4 @@ class Export(cli.Application):
             yaml.dump(result, f, default_flow_style=False)
         print("Made '{}'".format(PROJECT_YML))
         if not local.path(PROJECT_INFO_YML).exists():
-            print("Now customize the 'projectInfo' section and save.")
+            print("Now customize your {} and save.".format(PROJECT_YML))
