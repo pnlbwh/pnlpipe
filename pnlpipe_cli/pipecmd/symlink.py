ac975d57f8c2cdfe757e6cdb00335eff4d2f7b76
reckbo <reckbo@bwh.harvard.edu>
2017-08-07 20:36:37 +0000

    BUG: fix symlink

--- pnlpipe_cli/pipecmd/symlink.py
+++ pnlpipe_cli/pipecmd/symlink.py
@@ -12,6 +12,7 @@ import pnlpipe_config as config
 
 OUTDIR = local.path(config.OUTDIR)
 
+
 def rawgz_symlink(nhdr, symlink):
     nhdr = local.path(nhdr)
     symlink = local.path(symlink)
@@ -22,7 +23,8 @@ def rawgz_symlink(nhdr, symlink):
     rawgz = nhdr.with_suffix('.raw.gz')
     if not rawgz.exists():
         raise Exception("Can't find .raw.gz for '{}'".format(nhdr))
-    return { (symlink.dirname / rawgz.name) : rawgz }
+    return {(symlink.dirname / rawgz.name): rawgz}
+
 
 def bvec_symlink(nifti, symlink):
     symlink = local.path(symlink)
@@ -31,14 +33,13 @@ def bvec_symlink(nifti, symlink):
     bval = nifti.with_suffix('.bval', depth=2)
     result = {}
     if bvec.exists():
-        result[bvec] = symlink.dirname / bvec.name
+        result[bvec] = symlink.with_suffix('.bvec', depth=2)
     if bval.exists():
-        result[bval] = symlink.dirname / bval.name
+        result[bval] = symlink.with_suffix('.bval', depth=2)
     return result
 
 
-MULTI_SYMLINKS = {'.nhdr': rawgz_symlink,
-                  '.nii.gz': bvec_symlink}
+MULTI_SYMLINKS = {'.nhdr': rawgz_symlink, '.nii.gz': bvec_symlink}
 
 
 def make_symlink(src, symlink):
@@ -47,7 +48,8 @@ def make_symlink(src, symlink):
     for ext, extra_symlink_fn in MULTI_SYMLINKS.items():
         if src.endswith(ext):
             extra_symlinks = extra_symlink_fn(src, symlink)
-            for extra_symlink, extra_src in extra_symlinks.items():
+            for extra_src, extra_symlink in extra_symlinks.items():
+                print("Make: {} --> {}".format(extra_symlink.relative_to(local.cwd), extra_src.relative_to(local.cwd)))
                 extra_src.symlink(extra_symlink)
     provenance = local.path(src + '.provenance')
     if provenance.exists():
@@ -72,32 +74,42 @@ class SymLink(cli.Application):
 
         from plumbum.cmd import find
         for symlink in find(config.OUTDIR, '-type', 'l').split():
-            if local.path(symlink).name.startswith(pipename):
-                print("Remove {}".format(symlink))
-                os.unlink(symlink)
-                for ext, multi_symlink_fn in MULTI_SYMLINKS.items():
-                    if symlink.endswith(ext):
-                        multi_symlinks = multi_symlink_fn(os.path.realpath(symlink), symlink)
-                        for extra_symlink, _ in multi_symlinks.items():
-                            print("Remove extra {}".format(extra_symlink))
-                            if extra_symlink.exists():
-                                os.unlink(extra_symlink)
+            symlink = local.path(symlink)
+            if not symlink.name.startswith(pipename):
+                continue
+            if not symlink.exists():
+                continue
+            print("Remove {}".format(symlink))
+            os.unlink(symlink)
+            for ext, multi_symlink_fn in MULTI_SYMLINKS.items():
+                if symlink.endswith(ext):
+                    multi_symlinks = multi_symlink_fn(
+                        os.path.realpath(symlink), symlink)
+                    for extra_symlink, _ in multi_symlinks.items():
+                        if not extra_symlink.exists():
+                            continue
+                        print("Remove extra {}".format(extra_symlink))
+                        if extra_symlink.exists():
+                            os.unlink(extra_symlink)
 
         for paramid, combo, caseids in read_grouped_combos(pipename):
             print('')
             print("# Make symlinks for parameter combination {}".format(
                 paramid))
-            printVertical(combo)
+            printVertical(dict(combo, caseids=caseids))
             print('')
             for caseid in caseids:
                 pipeline = make_pipeline(pipename, combo, caseid)
                 for tag, node in pipeline.items():
+                    print(tag)
                     if not local.path(node.output()).exists():
                         continue
                     if not node.output().startswith(OUTDIR):
                         continue
-                    print("tag: {}".format(tag))
                     symlink = to_symlink(node, tag, pipename, paramid)
-                    print("Make '{}' --> '{}' ".format(symlink, node.output()))
+                    if symlink.exists():
+                        print("{} already exists".format(symlink))
+                        sys.exit()
+                    print("Make '{}' --> '{}' ".format(symlink.relative_to(local.cwd), node.output()))
                     symlink.dirname.mkdir()
                     make_symlink(node.output(), symlink)
