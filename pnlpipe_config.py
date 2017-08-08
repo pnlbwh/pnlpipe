956a9304aacd60c3bd23e4df7e0c55c8acdd258a
reckbo <reckbo@bwh.harvard.edu>
2017-08-08 12:10:50 +0000

    STY: add working config file to repo

--- pnlpipe_config.py
+++ pnlpipe_config.py
@@ -1,7 +1,7 @@
 OUTDIR = '_data'
 
 INPUT_KEYS = {
-    'caseid': '003_GNX_007',
+    'caseid_placeholder': '003_GNX_007',
     'dwi': '../testdata/INTRuST/003_GNX_007-dwi-Ed.nhdr',
     'dwiraw': '../testdata/INTRuST/003_GNX_007-dwi-Ed.nhdr',
     'dwiharm': '../testdata/INTRuST/003_GNX_007_hmz_iter1.nhdr',
