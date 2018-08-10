ab6d6c32cb10ab38defd920a4129c7e4c6b525ff
Billah, Tashrif <tbillah@bwh.harvard.edu>
2018-08-10 16:06:02 -0400

    Installation instruction updated

--- pnlpipe_config.py
+++ pnlpipe_config.py
@@ -3,7 +3,7 @@ OUTDIR = '_data'
 intrust = {
     'caseid_placeholder': '003_GNX_007',
     'dwied': '/home/tb571/Downloads/INTRuST/003_GNX_007/diff/003_GNX_007-dwi-Ed.nhdr',
-    'dwi': '/home/tb571/Downloads/INTRuST/003_GNX_007/diff/003_GNX_007-dwi.nhdr',
+    'dwi': '/home/tb571/Downloads/INTRuST/003_GNX_007/raw/003_GNX_007-dwi.nhdr',
     'dwiharm': '/home/tb571/Downloads/INTRuST/003_GNX_007/diff/003_GNX_007_hmz_iter1.nhdr',
     't1': '/home/tb571/Downloads/INTRuST/003_GNX_007/raw/003_GNX_007-t1w.nhdr',
     't2': '/home/tb571/Downloads/INTRuST/003_GNX_007/raw/003_GNX_007-t2w.nhdr'
