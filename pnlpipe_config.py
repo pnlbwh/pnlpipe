3bb8b9da1c0b820bd53a5a1d7966143d561fbbe8
Billah, Tashrif <tbillah@bwh.harvard.edu>
2019-03-21 12:50:12 -0400

    default conf

---
+++ pnlpipe_config.py
@@ -0,0 +1,24 @@
+OUTDIR = '_data' # pnlpipe/_data, alternatively you may specify an absolute path
+
+# diverting the temporary directory to avoid space shortage in shared /tmp
+# set to None to write to default /tmp
+import os
+TMPDIR = os.path.join(os.environ['HOME'],'tmp')
+# if the path you are setting doesn't exist, it will point to default /tmp
+TMPDIR = TMPDIR if os.path.exists(TMPDIR) else '/tmp'
+
+# an input path is found by looking up its key in INPUT_KEYS
+# value of each key is returned after substituting a caseid"
+# give proper paths for one caseid as your files are stored in your system
+INTRuST = {
+    'caseid_placeholder': '003_GNX_007',
+    'dwi': '/data/pnl/INTRuST/003_GNX_007/raw/003_GNX_007-dwi.nhdr',
+    't1': '/data/pnl/INTRuST/003_GNX_007/raw/003_GNX_007-t1w.nhdr',
+    't2': '/data/pnl/INTRuST/003_GNX_007/raw/003_GNX_007-t2w.nhdr'
+}
+INPUT_KEYS = INTRuST
+
+# every time you input a different set of std.params/epi.params,
+# give the following a different name
+# so you are able to compare tract-summaries among parameters
+PROJECT_ALG_NAME = 'usrAlg '
