84d6fb8fbfa5df79846b30ce1dddff37845986c8
Billah, Tashrif <tbillah@bwh.harvard.edu>
2019-06-02 14:15:22 +0000

    update nifti masking for FSL 6.0.1

--- pnlscripts/bet.py
+++ pnlscripts/bet.py
@@ -3,7 +3,9 @@ from util.scripts import bse_py
 from plumbum.cmd import bet, ConvertBetweenFileFormats
 from plumbum import cli, FG
 from util import Nrrd, ExistingNrrd, ExistingNrrdOrNifti, TemporaryDirectory
-import logging
+from util.scripts import bse_py
+
+import logging, sys
 
 def nifti(f):
     return '.nii' in f.suffixes
@@ -29,7 +31,6 @@ class App(cli.Application):
     def main(self):
         if self.out.exists() and not self.force:
             logging.error("'{}' already exists, use --force to force overwrite")
-            import sys
             sys.exit(1)
         with TemporaryDirectory() as tmpdir:
 
@@ -39,8 +40,14 @@ class App(cli.Application):
                 bsenii = tmpdir / 'bse.nii.gz'
                 ConvertBetweenFileFormats[bse, bsenii] & FG
                 bet[bsenii, tmpdir / 'dwi', '-m', '-f', self.threshold] & FG
-            else: #nifti
-                bet[self.dwi, tmpdir / 'dwi', '-m', '-f', self.threshold] & FG
+            else:
+                # nifti
+                # FSL 6.0.1, unlike <=5.0.11, doesn't create only one mask corresponding to b0 extracted from given DWI
+                # hence we need to provide baseline image to bet like we did for nrrd above
+                bsenii = tmpdir / 'bse.nii.gz'
+                bse_py['-i', self.dwi, '-o', bsenii] & FG
+                bet[bsenii, tmpdir / 'dwi', '-m', '-f', self.threshold] & FG
+
             ConvertBetweenFileFormats[tmpdir / 'dwi_mask.nii.gz', self.out] & FG
 
 if __name__ == '__main__':
